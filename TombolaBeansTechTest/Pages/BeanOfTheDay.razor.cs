668000bd8cfa5612ca91e4b293f83c8b6fb355e6
BenRamsden30 <ben-ramsden@hotmail.com>
2025-03-09 14:30:43 +0000

    razor and cs aren't talking

--- TombolaBeansTechTest/Pages/BeanOfTheDay.razor.cs
+++ TombolaBeansTechTest/Pages/BeanOfTheDay.razor.cs
@@ -4,41 +4,40 @@ using TombolaBeansTechTest.Services;
 
 namespace TombolaBeansTechTest.Pages
 {
-    public partial class BeanOfTheDay : ComponentBase
+public partial class BeanOfTheDay
+{
+    // The selected CoffeeBean for the "Bean of the Day"
+    private CoffeBeans selectedBean;
+
+    // Injecting the CoffeeBeansService
+    [Inject] private CoffeBeanService CoffeeBeansService { get; set; }
+
+    // Injecting the NavigationManager for navigation
+    [Inject] private NavigationManager NavigationManager { get; set; }
+
+    // This method is invoked when the component is initialized
+    protected void OnInitialized()
     {
-        // Injecting the CoffeeBeansService
-        [Inject] private CoffeBeanService CoffeeBeansService { get; set; }
-
-        // Injecting the NavigationManager for navigation
-        [Inject] private NavigationManager NavigationManager { get; set; }
-
-        // The selected CoffeeBean for the "Bean of the Day"
-        private CoffeBeans selectedBean;
-
-        // This method is invoked when the component is initialized
-        protected void OnInitialized()
-        {
-            // Get the random "Bean of the Day" and ensure it's not the same as the previous day
-            selectedBean = GetBeanOfTheDay();
-        }
-
-        // Function to get the "Bean of the Day"
-        private CoffeBeans GetBeanOfTheDay()
-        {
-            var beans = CoffeeBeansService.GetAllBeans(); // Get the list of beans
-            var random = new Random();
-
-            // Select a random coffee bean
-            var todaysBean = beans[random.Next(beans.Count)];
-
-            return todaysBean;
-        }
-
-        // Handler to navigate to more details about the current Bean of the Day
-        private void ShowMoreDetails()
-        {
-            // Navigate to a page showing more details of the current coffee bean
-            NavigationManager.NavigateTo($"/coffeebean/{selectedBean.Name}");
-        }
+        // Get the random "Bean of the Day" and ensure it's not the same as the previous day
+        selectedBean = GetBeanOfTheDay();
     }
-}
+
+    // Function to get the "Bean of the Day"
+    private CoffeBeans GetBeanOfTheDay()
+    {
+        var beans = CoffeeBeansService.GetAllBeans(); // Get the list of beans
+        var random = new Random();
+
+        // Select a random coffee bean
+        var todaysBean = beans[random.Next(beans.Count)];
+
+        return todaysBean;
+    }
+
+    // Handler to navigate to more details about the current Bean of the Day
+    private void ShowMoreDetails()
+    {
+        // Navigate to a page showing more details of the current coffee bean
+        NavigationManager.NavigateTo($"/coffeebean/{selectedBean.Name}");
+    }
+}}
