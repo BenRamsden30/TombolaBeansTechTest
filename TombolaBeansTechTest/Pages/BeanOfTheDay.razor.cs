3e3034e325164b9a210b58e459c91ad6494134d0
BenRamsden30 <ben-ramsden@hotmail.com>
2025-03-09 11:05:48 +0000

    Added the rotating beans portion of code, next step test front end and give a shot at writing some tests for it then address back end

--- TombolaBeansTechTest/Pages/BeanOfTheDay.razor.cs
+++ TombolaBeansTechTest/Pages/BeanOfTheDay.razor.cs
@@ -1,7 +1,42 @@
 using Microsoft.AspNetCore.Components;
+using TombolaBeansTechTest.Models;
 
 namespace TombolaBeansTechTest.Pages;
 
-public partial class BeanOfTheDay : ComponentBase
+using System.Text.Json;
+
+public class CoffeeBeanService
 {
+    private List<CoffeBeans> _beans;
+    private static int _lastBeanId = -1; // Stores last selected bean ID
+
+    public CoffeeBeanService()
+    {
+        string json = File.ReadAllText("wwwroot/data/coffeeBeans.json");
+        _beans = JsonSerializer.Deserialize<List<CoffeBeans>>(json);
+    }
+
+    public List<CoffeBeans> GetAllBeans() => _beans;
+
+    public CoffeBeans GetBeanById(int id) => _beans.FirstOrDefault(b => b.Id == id);
+
+    public CoffeBeans GetBeanOfTheDay()
+    {
+        if (_beans == null || _beans.Count == 0)
+            return null;
+
+        var random = new Random();
+        CoffeBeans selectedBean;
+
+        // Ensure the new bean is different from the last one
+        do
+        {
+            selectedBean = _beans[random.Next(_beans.Count)];
+        } while (selectedBean.Id == _lastBeanId);
+
+        // Store the selected bean ID
+        _lastBeanId = selectedBean.Id;
+
+        return selectedBean;
+    }
 }
